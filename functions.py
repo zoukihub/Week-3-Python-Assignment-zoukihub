403b1a37e3d1b37f7572c419c54b5f8188674aab
zoukihub <moraabochere@gmail.com>
2024-09-09 22:52:24 +0300

    Calling the function and getting the final price

--- functions.py
+++ functions.py
@@ -9,4 +9,5 @@ def calculate_discount(price, discount_percent):
 try:
     original_price = float(input("Enter the original price of the item: "))
     discount_percent = float(input("Enter the discount percentage: "))
-    
+
+    final_price = calculate_discount(original_price, discount_percent)
