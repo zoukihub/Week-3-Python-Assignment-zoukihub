bc9287f6a5e28537992c0d9a9e97789a21fe4f41
zoukihub <moraabochere@gmail.com>
2024-09-09 22:46:55 +0300

    Function to calculate the final price after discount

---
+++ functions.py
@@ -0,0 +1,7 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = (discount_percent / 100) * price
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
