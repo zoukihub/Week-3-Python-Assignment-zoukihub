8307ba1a67c2700cc8de7eadb2fb9fada2c09f2c
zoukihub <moraabochere@gmail.com>
2024-09-09 22:56:50 +0300

    Output the result

--- functions.py
+++ functions.py
@@ -11,3 +11,10 @@ try:
     discount_percent = float(input("Enter the discount percentage: "))
 
     final_price = calculate_discount(original_price, discount_percent)
+
+    if discount_percent >= 20:
+        print(f"The final price after a {discount_perrcent}% discount is: ${final_price:.2f}")
+    else:
+        print(f"No discount applied. The original price is: ${original_price:.2f}")
+except ValueError:
+    print("Please enter valid numerical values for price and discount.")
